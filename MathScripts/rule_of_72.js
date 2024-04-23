ad4d4e3ef2c74851a1acd45673dd6bcd6b19ff8d
Julian Pompey <jupompey00@gmail.com>
2024-04-23 09:51:38 -0400

    slight fix on rule of 72.js

--- MathScripts/rule_of_72.js
+++ MathScripts/rule_of_72.js
@@ -5,5 +5,5 @@
 var interestRate = 3;
 var bankAccount = 24;
 
-console.log("At a " + interestRate + "% interest rate, your savings account will be worth $" + (bankAccount * 2)
-+ " in about " + (72/interestRate) + " Years" )
+console.log("At a " + interestRate + "% interest rate, your savings account will be worth $" + (bankAccount * 2).toFixed(2)
++ " in about " + (72/interestRate).toFixed(1) + " Years" )
