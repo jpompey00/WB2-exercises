e09dd0d2c0bf80a0727746dad0b5dd958b94c7a8
Julian Pompey <jupompey00@gmail.com>
2024-04-23 14:30:37 -0400

    checkpoint2

--- MathScripts/tiles.js
+++ MathScripts/tiles.js
@@ -1,15 +1,20 @@
 "use strict"
 
 //CHANGE BASED ON ROOM DIMENSIONS
-var roomLength = 10;
+var roomLength = 10;100*.1
 var roomWidth = 10;
 
 //Formula for the room area
 var roomArea = roomLength * roomWidth;
 
+//getting 10% more tiles than needed
+var extrasNeeded = roomArea * .10
+
+
 //Formula determining the amount of boxes needed
-var boxesNeeded = Math.ceil(roomArea / 12);
+var boxesNeeded = Math.ceil((roomArea + extrasNeeded) / 12);
+
 
 
 //Output
-console.log(`For a room with an area of ${roomArea} feet, you need ${boxesNeeded} boxes`)
+console.log(`For a room with an area of ${roomArea} feet, and getting ${extrasNeeded} extra feet of tile, you need ${boxesNeeded} boxes`)
