2257be28b448ec79c7cafaf44e7c098f990ba663
Julian Pompey <jupompey00@gmail.com>
2024-04-23 14:21:35 -0400

    checkpoint 2

--- MathScripts/rentals.js
+++ MathScripts/rentals.js
@@ -1,14 +1,23 @@
 "use strict"
 
+//CHANGE Based on people on tour 
 var peopleOnTour = 38;
 
-var vansNeeded = Math.ceil(peopleOnTour/15);
+//How many people can fit on the bus
+var spaceOnBus = 15;
 
+//finds the vans needed by diving and rounding up result of the people divided by the space on the bus
+var vansNeeded = Math.ceil(peopleOnTour/spaceOnBus);
+
+//The price per day to rent a bus
 const PRICE_TO_RENT = 250;
 
+//Calculates the cost needed per day
 var costPerDay = vansNeeded * PRICE_TO_RENT;
 
+//Calculates how much each person will need to pay per day
 var costPerPerson = (costPerDay/peopleOnTour).toFixed(2);
 
 
+//output
 console.log(`There are ${peopleOnTour} people on tour. ${vansNeeded} vans are needed.\nThe cost to rent the vans per day is $${costPerDay}\nThis will cost $${costPerPerson} per person`)
--- MathScripts/tiles.js
+++ MathScripts/tiles.js
@@ -1,10 +1,15 @@
 "use strict"
 
+//CHANGE BASED ON ROOM DIMENSIONS
 var roomLength = 10;
 var roomWidth = 10;
 
+//Formula for the room area
 var roomArea = roomLength * roomWidth;
 
+//Formula determining the amount of boxes needed
 var boxesNeeded = Math.ceil(roomArea / 12);
 
+
+//Output
 console.log(`For a room with an area of ${roomArea} feet, you need ${boxesNeeded} boxes`)
