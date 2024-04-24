277090472898478930ee5ee761416f2f3a63aafa
Julian Pompey <jupompey00@gmail.com>
2024-04-24 11:03:28 -0400

    checkpoint 2 04/24

--- NumericConversions/conversion_tests.js
+++ NumericConversions/conversion_tests.js
@@ -1,18 +1,22 @@
 //Description: This script tests various numeric conversion techniques
 //
 //Author: Julian Pompey
+"use strict";
 
-
- a = "  101.1   ";
+var a = "  101.1   ";
 var b = "55";
 var c = "402 Stevens";
 var d = "Number 5   ";
 
 
+//Parseing the A Variable
 console.log(`Expression ${a} | ParseInt result= ${parseInt(a)} |  parseFloat result= ${parseFloat(a)} |  numberResult= ${Number(a)} |  UnaryResult = ${+a}`)
 
+//Parseing the B Variable
 console.log(`Expression ${b} |  ParseInt result= ${parseInt(b)} |  parseFloat result= ${parseFloat(b)} |  numberResult= ${Number(b)} |  UnaryResult= ${+b}`)
 
+//Parseing the C Variable
 console.log(`Expression ${c} |  ParseInt result= ${parseInt(c)} |  parseFloat result= ${parseFloat(c)} |  numberResult= ${Number(c)} |  UnaryResult= ${+c}`)
 
+//Parseing the D Variable
 console.log(`Expression ${d} |  ParseInt result= ${parseInt(d)} |  parseFloat result= ${parseFloat(d)} |  numberResult= ${Number(d)} |  UnaryResult= ${+d}`)
